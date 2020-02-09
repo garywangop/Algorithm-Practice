a1eca0d2b090351a37e98bde791d5c1831957005
garywangop <garywangop@gmail.com>
2020-02-09 16:55:21 -0500

    Binary Tree Path Sum To Target I

--- binaryTree/MoreBinaryTrees.java
+++ binaryTree/MoreBinaryTrees.java
@@ -121,21 +121,44 @@ public class MoreBinaryTrees {
 	 * 4
 	 */
 	public int maxPathSum3(TreeNode root) {
-	    int[] max = new int[]{Integer.MIN_VALUE};
-	    helper140(root, max);
-	    return max[0];
-	  }
+		int[] max = new int[] { Integer.MIN_VALUE };
+		helper140(root, max);
+		return max[0];
+	}
 
-	  private int helper140(TreeNode root, int[] max) {
-	    if (root == null) {
-	      return 0;
-	    }
-	    int left = helper140(root.left, max);
-	    int right = helper140(root.right, max);
-	    left = left < 0 ? 0 : left;
-	    right = right < 0 ? 0 : right;
-	    max[0] = Math.max(max[0], root.value + Math.max(left, right));
-	    return Math.max(left, right) + root.value;
-	  }
+	private int helper140(TreeNode root, int[] max) {
+		if (root == null) {
+			return 0;
+		}
+		int left = helper140(root.left, max);
+		int right = helper140(root.right, max);
+		left = left < 0 ? 0 : left;
+		right = right < 0 ? 0 : right;
+		max[0] = Math.max(max[0], root.value + Math.max(left, right));
+		return Math.max(left, right) + root.value;
+	}
+
+	/*
+	 * 440. Binary Tree Path Sum To Target I
+	 * 
+	 * Given a binary tree and a target sum, determine if the tree has a
+	 * root-to-leaf path such that adding up all the values along the path equals
+	 * the given target.
+	 * 
+	 * Example: Given the below binary tree and target = 16,
+	 * 
+	 * 5 / \ 4 8 / / \ 1 3 4 / \ \ 7 2 1 return true, as there exist a root-to-leaf
+	 * path 5-8-3 which sum is 16.
+	 */
+
+	public boolean exist(TreeNode root, int target) {
+		if (root == null) {
+			return false;
+		}
+		if (root.left == null && root.right == null) {
+			return root.value == target;
+		}
+		return exist(root.left, target - root.value) || exist(root.right, target - root.value);
+	}
 
 }
